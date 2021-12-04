a364e531aaa6460bcb6c570d63e61438b1802cea
david24022 <david.24022@yahoo.com.tw>
2021-12-04 17:02:02 +0800

    修改sql(member)新增commit

--- doc/00_sql/init_sql.sql
+++ doc/00_sql/init_sql.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `member` (
-  `member_id` int NOT NULL AUTO_INCREMENT,/*會員編號*/
-  `member_account` varchar(16) NOT NULL,/*會員帳號*/
-  `member_password` varchar(16) NOT NULL,/*會員密碼*/
-  `creat_time` datetime DEFAULT NULL,/*創建時間*/
-  `member_state` int NOT NULL,/*會員狀態*/
+  `member_id` int NOT NULL AUTO_INCREMENT comment '會員編號',
+  `member_account` varchar(16) NOT NULL comment '會員帳號',
+  `member_password` varchar(16) NOT NULL comment '會員密碼',
+  `creat_time` datetime DEFAULT NULL comment '創建時間',
+  `member_state` int NOT NULL comment '會員狀態 0=下線,1=上線,2=刪除帳號',
   PRIMARY KEY (`member_id`)
 );
 
