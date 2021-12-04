959e56d4d12f5f8f3e2e05a9f05bb041f3680f41
david24022 <david.24022@yahoo.com.tw>
2021-12-04 16:55:41 +0800

    update sql(member)

--- doc/00_sql/init_sql.sql
+++ doc/00_sql/init_sql.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `EXAMPLE`.`member` (
-`member_id` int(20) NOT NULL AUTO_INCREMENT,
-  `member_account` varchar(16) NOT NULL,
-  `member_password` varchar(16) NOT NULL,
-	`creat_time` date,
-	`member_state` int(5) NOT NULL,
+CREATE TABLE `member` (
+  `member_id` int NOT NULL AUTO_INCREMENT,/*會員編號*/
+  `member_account` varchar(16) NOT NULL,/*會員帳號*/
+  `member_password` varchar(16) NOT NULL,/*會員密碼*/
+  `creat_time` datetime DEFAULT NULL,/*創建時間*/
+  `member_state` int NOT NULL,/*會員狀態*/
   PRIMARY KEY (`member_id`)
 );
 
-insert into EXAMPLE.member (member_account ,member_password,creat_time,member_state) values ('test123','123465','2021/12/4',0)
-insert into EXAMPLE.member (member_account ,member_password,creat_time,member_state) values ('test','123','2021/12/5',0)
+insert into EXAMPLE.member (member_account ,member_password,creat_time,member_state) values ('test123','123465','2021-12-04 12:00',0)
+insert into EXAMPLE.member (member_account ,member_password,creat_time,member_state) values ('test','123','2021-12-5 12:30',0)
